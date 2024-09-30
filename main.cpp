50bb9512afbed0839244e5e5b9ec42ed452c9dcd
AchroDev <achrodev@proton.me>
2024-09-30 17:29:38 -0500

    Added example code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <string>
+
+using String = std::string;
+
+// The "new" keyword in C++
+
+class Entity
+{
+private:
+    String m_Name;
+
+public:
+    Entity() : m_Name("Unknown") {}
+    Entity(const String &name) : m_Name(name) {}
+
+    const String &GetName() const { return m_Name; }
+};
 
 int main()
 {
