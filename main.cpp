beabd3ba0169408e214c01ad4370268e4fe30215
AchroDev <achrodev@proton.me>
2024-09-30 23:17:08 -0500

    Added placement new example

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@ int main()
     Entity *e = new Entity();                     // new heap allocation using the Entity
     Entity *e = (Entity *)malloc(sizeof(Entity)); // This line of code is essentially doing the same thing as the above line, except it doesn't call the constructor
 
+    // Placement new example, calling the constructor and initializing the Entity in a specific memory address
+    Entity *p = new (b) Entity();
+
     delete a2;
     free(a2);   // Like above with 'malloc', delete calls the C function free() to free the memory. Except it doesn't call the deconstructor
     delete[] b; // If you call a "new" instance with [] then you must delete with the brackets as well.
